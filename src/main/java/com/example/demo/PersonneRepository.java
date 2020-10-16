3cb00b168e8ab6589fd02b11280e996f306db67e
Josue <vidrequin.pro@outlook.fr>
2020-10-16 19:56:24 +0200

    second version

--- src/main/java/com/example/demo/PersonneRepository.java
+++ src/main/java/com/example/demo/PersonneRepository.java
@@ -2,4 +2,14 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonneRepository  extends CrudRepository<Personne, Integer>{ }
+import java.util.Optional;
+
+public interface PersonneRepository  extends CrudRepository<Personne, Integer>{
+     void deletePersonne(Personne p);
+     Iterable<Personne> searchPersonne();
+     Personne createPersonne(Personne p);
+    Optional< Personne > findById(Long id);
+
+
+
+}
--- src/main/java/com/example/demo/PersonneRessource.java
+++ src/main/java/com/example/demo/PersonneRessource.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
 
 //Pour le lien en localhost
 @Path("personnes")
@@ -32,5 +35,17 @@ public class PersonneRessource{
     @Path("/delete")
     public void deletePersonne(Personne p) {
         personneRepository.delete(p);
+    }
+
+   /* @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/one/{id}")
+    public Optional<Personne> searchOnePersonne(@PathVariable int id) {
+        return personneRepository.findById(id);
+    }*/
+
+    public Optional <Personne> findById(Long id) {
+        return personneRepository.findById(id);
     }
 }
